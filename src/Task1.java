d2cfcb0b6c1e0332deb634b40a0d8a8555f73cd0
martinshare <mbrzeczek150@gmail.com>
2019-10-22 17:10:14 +0200

    Aded poc of solution.

--- src/Task1.java
+++ src/Task1.java
@@ -1,6 +1,48 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Task1 {
 
+
+    private int solution(int N) {
+        String binaryValue = Integer.toBinaryString(N);
+
+        System.out.println(binaryValue);
+
+        int countContainsOnes = 0;
+        int result = 0;
+        char numbers[] = binaryValue.toCharArray();
+
+        for (char number : numbers) {
+            if (number == '1')
+                countContainsOnes++;
+        }
+
+        System.out.println("Contains ones number: " + countContainsOnes);
+
+        int[] resultNumbers = new int[countContainsOnes];
+
+        if (countContainsOnes > 1) {
+            for (int i = 0; i < countContainsOnes - 1; i++) {
+                for (int j = 1; j < numbers.length; j++) {
+                    if (numbers[j] == '0') {
+                        result++;
+                    }else
+                        break;
+                }
+                resultNumbers[i] = result;
+                System.out.println(result);
+                result = 0;
+            }
+            return Arrays.stream(resultNumbers).max().getAsInt();
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int result = new Task1().solution(1041);
+
     }
 }

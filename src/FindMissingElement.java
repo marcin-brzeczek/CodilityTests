fcf5497e014184adbc0d2751ac46e22f00d3c299
marcinbrzeczek <mbrzeczek150@gmail.com>
2019-11-18 14:34:36 +0100

    Added next codility test.

--- src/FindMissingElement.java
+++ src/FindMissingElement.java
@@ -6,20 +6,22 @@ public class FindMissingElement {
         Arrays.sort(A);
         int result = 0;
         int size = A.length;
-        if(A.length == 1 ){
-            return  (A[0] > 1)  ? A[0] - 1 : A[0] + 1;
+        if (A.length == 0 || A[0] != 1)
+            return 1;
+        if (A.length == 1) {
+            return A[0] + 1;
         }
-        for (int i = 0; i < size -1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             if (A[i + 1] - A[i] != 1) {
                 return A[i] + 1;
-            }else
-                result = A[i] +2;
+            } else
+                result = A[i] + 2;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int result = new FindMissingElement().solution(new int[]{3});
+        int result = new FindMissingElement().solution(new int[]{1, 2, 3, 4, 5, 7});
         System.out.println(result);
     }
 }
---
+++ src/TapeEqulibrum.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class TapeEqulibrum {
+
+    public int solution(int A[]) {
+        int partOne = 0;
+        int partTwo = 0;
+        int difference;
+        int N = A.length;
+        int arrayLength = N - 1;
+        int[] arrayPartOne = new int[arrayLength];
+        int[] arrayPartTwo = new int[arrayLength];
+        int[] arrayDifferences = new int[arrayLength];
+
+        for (int P = 1; P < N; P++) {
+            partOne = partOne + A[P - 1];
+            arrayPartOne[P - 1] = partOne;
+
+            partTwo = partTwo + A[N - P];
+            arrayPartTwo[N - P - 1] = partTwo;
+        }
+
+        for (int i = 0; i < arrayLength; i++) {
+            difference = arrayPartOne[i] - arrayPartTwo[i];
+            if (difference < 0)
+                difference = difference * -1;
+            arrayDifferences[i] = difference;
+        }
+
+        if (arrayDifferences.length == 1) {
+            return arrayDifferences[0];
+        }
+        Arrays.sort(arrayDifferences);
+        return arrayDifferences[0];
+    }
+
+    public static void main(String[] args) {
+        int result = new TapeEqulibrum().solution(new int[]{3, 1, 2, 4, 3});
+
+        System.out.println(result);
+    }
+}
